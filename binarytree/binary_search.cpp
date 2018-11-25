79d9c1a129c34a4c891ec581c1b98dccc2827d06
chacha <867571123@qq.com>
2018-11-25 11:13:16 +0800

    Add search in rotated binary tree

--- binarytree/binary_search.cpp
+++ binarytree/binary_search.cpp
@@ -2,7 +2,7 @@
  * @Author: Chacha 
  * @Date: 2018-11-24 19:05:12 
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-11-25 00:08:37
+ * @Last Modified time: 2018-11-25 11:09:01
  */
 
 /**
@@ -119,6 +119,64 @@ vector<int> binarySearchForRange(vector<int> nums, int target) {
 
     return bound;
 }
+
+/***********************************************************************************
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * Source: https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Example 1:
+ *  Input: nums = [4,5,6,7,0,1,2], target = 0
+ *  Output: 4
+ * Example 2:
+ *  Input: nums = [4,5,6,7,0,1,2], target = 3
+ *  Output: -1
+************************************************************************************/
+int searchInRotatedBinaryTree(vector<int> nums, int target) {
+    if (nums.empty() == true) {
+        return -1;
+    }
+
+    int start = 0;
+    int end = nums.size() -1;
+    int mid; 
+    
+    while(start + 1 < end){
+        // Prevent (left + right) overflow
+        mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[start] < nums[mid]) {
+            // situation 1, numbers between start and mid are sorted
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        } else {
+            // situation 2, numbers between mid and end are sorted
+            if (nums[mid] < garget && target <= nums[end]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+    }
+
+    if (nums(start) == target) {
+        return start;
+    }
+    if (nums[end] == target) {
+        return end;
+    }
+
+    return -1;
+}
+
 
 void printVector(vector<int> &vec) {
     for(int i= 0; i < vec.size(); i++) {
