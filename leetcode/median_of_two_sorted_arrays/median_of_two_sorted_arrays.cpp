5d49cc6d16760bad4419bf41fe9412cf7672b92b
chacha <867571123@qq.com>
2018-12-02 22:43:08 +0800

    Update median of two sorted arrays

--- leetcode/median_of_two_sorted_arrays/median_of_two_sorted_arrays.cpp
+++ leetcode/median_of_two_sorted_arrays/median_of_two_sorted_arrays.cpp
@@ -1,10 +1,28 @@
-/*
+/**
  * @Author: Chacha 
  * @Date: 2018-11-30 16:49:55 
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-12-01 22:28:42
+ * @Last Modified time: 2018-12-02 22:42:29
  */
 
 #include <iostream>
 #include <vector>
 using namespace std;
+
+/***********************************************************************************
+ * There are two sorted arrays A and B of size m and n respectively. 
+ * Find the median of the two sorted arrays.
+ * You may assume nums1 and nums2 cannot be both empty.
+ * 
+ * Example 1:
+ *  nums1 = [1, 3]
+ *  nums2 = [2]
+ *  The median is 2.0
+ * 
+ * Example 2:
+ *  nums1 = [1, 2]
+ *  nums2 = [3, 4]
+ *  The median is (2 + 3)/2 = 2.5
+ * 
+ * Source: https://leetcode.com/problems/median-of-two-sorted-arrays/
+************************************************************************************/
