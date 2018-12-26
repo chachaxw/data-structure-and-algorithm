8cffb469300a5b2a88c3800b9115c60680a989d7
chacha <867571123@qq.com>
2018-12-26 23:44:29 +0800

    Add shell sorting function

--- basic_sorting/insertion_sort.cpp
+++ basic_sorting/insertion_sort.cpp
@@ -2,18 +2,17 @@
  * @Author: Chacha 
  * @Date: 2018-12-20 22:10:30 
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-12-21 23:10:37
- */
-
-/**
- * Bubble Sorting 
- * Source: https://zh.wikipedia.org/wiki/插入排序
+ * @Last Modified time: 2018-12-26 23:41:11
  */
 
 #include <iostream>
 using namespace std;
 template<typename T> //整数或浮点数皆可使用,若要使用类(class)或结构体(struct)时必须重载大于(>)运算符
 
+/**
+ * Insertion Sorting 
+ * Source: https://zh.wikipedia.org/wiki/插入排序
+ */
 void insertionSort(T arr[], int len) {
     
     for(int i = 1; i < len; i++) {
@@ -26,7 +25,28 @@ void insertionSort(T arr[], int len) {
         }
         arr[j + 1] = key;
     }
+}
+
+/**
+ * Shell Sorting 
+ * Source: https://zh.wikipedia.org/wiki/希尔排序
+ */
+void shellSort(int arr[], int len) {
+    int gap, i, j;
+    int temp;
+
+    for(gap = len >> 1; gap >0; gap >>= 1) {
     
+        for(i = gap;i < len; i++) {
+            temp = arr[i];
+            
+            for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap) {
+                arr[j + gap] = arr[j];
+            }
+            arr[j + gap] = temp;
+        }
+    }
+
 }
 
 int main() {
---
+++ basic_sorting/merge_sort.cpp
@@ -0,0 +1,18 @@
+/**
+ * @Author: Chacha 
+ * @Date: 2018-12-25 22:53:20 
+ * @Last Modified by: Chacha
+ * @Last Modified time: 2018-12-25 22:58:47
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int mergeSort(vector<int> &nums) {
+    
+}
+
+int main() {
+    return 0;
+}
