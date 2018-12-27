e6d04c2b5713688b16232660fdce166d53017623
chacha <867571123@qq.com>
2018-12-27 23:08:57 +0800

    Fixed shell sort function bug

--- basic_sorting/insertion_sort.cpp
+++ basic_sorting/insertion_sort.cpp
@@ -2,11 +2,12 @@
  * @Author: Chacha 
  * @Date: 2018-12-20 22:10:30 
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-12-26 23:41:11
+ * @Last Modified time: 2018-12-27 22:03:09
  */
 
 #include <iostream>
 using namespace std;
+
 template<typename T> //整数或浮点数皆可使用,若要使用类(class)或结构体(struct)时必须重载大于(>)运算符
 
 /**
@@ -31,13 +32,14 @@ void insertionSort(T arr[], int len) {
  * Shell Sorting 
  * Source: https://zh.wikipedia.org/wiki/希尔排序
  */
-void shellSort(int arr[], int len) {
+template<typename T>
+void shellSort(T arr[], int len) {
     int gap, i, j;
-    int temp;
+    T temp;
 
-    for(gap = len >> 1; gap >0; gap >>= 1) {
+    for(gap = len >> 1; gap > 0; gap >>= 1) {
     
-        for(i = gap;i < len; i++) {
+        for(i = gap; i < len; i++) {
             temp = arr[i];
             
             for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap) {
@@ -53,7 +55,8 @@ int main() {
     int arr[] = { 2, 6, 4, 20, 9, 8 };
     int len = sizeof(arr)/sizeof(int);
 
-    insertionSort(arr, len);
+    // insertionSort(arr, len);
+    shellSort(arr, len);
 
     for (int i = 0; i < len; i++) {
         cout << arr[i] << ' ';
