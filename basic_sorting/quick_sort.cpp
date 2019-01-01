2ce826bbfd7f27f7a73ebbe0d6c2b6b6d4380e69
chacha <867571123@qq.com>
2019-01-01 22:59:41 +0800

    Update quick sort function

--- basic_sorting/quick_sort.cpp
+++ basic_sorting/quick_sort.cpp
@@ -2,7 +2,7 @@
  * @Author: Chacha 
  * @Date: 2018-12-29 23:03:23 
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-12-30 14:21:31
+ * @Last Modified time: 2019-01-01 21:25:38
  */
 
 #include <iostream>
@@ -16,9 +16,19 @@ using namespace std;
  */
 
 // Pseudocode
-function(array) {
-    
-}
+// function quicksort(arr) {
+//     var list less, pivotList, greater
+//     if length(arr) <= 1 {
+//         return arr;
+//     } else {
+//         select a pivot value pivot from arr
+//         for each x in arr except the pivot element
+//             if x < pivot then add x to less
+//             if x >= pivot then add x to greater
+//         add pivot to pivotList
+//         return concatenate(quicksort(less), pivotList, quickSort(greater));
+//     }
+// }
 
 int quickSort(vector<int> &nums) {
     
