c2363034b9c9b07d78957bbe08f03d75bcbd73e9
chacha <867571123@qq.com>
2021-03-05 10:41:24 +0800

    feat: add array data structure

---
+++ basic_data_structure/array/array.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: Chacha
+ * @Date: 2021-03-05 00:04:18
+ * @Last Modified by: Chacha
+ * @Last Modified time: 2021-03-05 00:04:56
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int nums[5] = {2, 3, 1, 0, 2};
+
+    printf("This is an array\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << nums[i] << ' ';
+    }
+
+    /* code */
+
+    return 0;
+}
