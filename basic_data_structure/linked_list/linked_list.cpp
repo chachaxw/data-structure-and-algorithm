6a9b52bd38173c98ab41f1e96313b8aa635c5ff5
chacha <867571123@qq.com>
2021-03-05 10:45:50 +0800

    feat: update linked list data structure

--- basic_data_structure/linked_list/linked_list.cpp
+++ basic_data_structure/linked_list/linked_list.cpp
@@ -1,48 +1,65 @@
-/**
+/*
  * @Author: Chacha
  * @Date: 2018-12-30 13:12:05
  * @Last Modified by: Chacha
- * @Last Modified time: 2018-12-30 17:00:28
+ * @Last Modified time: 2021-03-05 10:45:20
  */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /**
- * Struct definition for singly-linked list(单向链表)
+ * Struct definition for singly-linked list(单向链表结构体声明)
  * Source: https://zh.wikipedia.org/wiki/链表
  */
-struct ListNodeA {
-    int val;
-    ListNodeA *next;
+struct ListNodeA
+{
+    int val;         // 节点值
+    ListNodeA *next; // 后继节点引用
 
     ListNodeA(int x) : val(x), next(NULL) {}
 };
 
 /**
- * Class definition for singly-linked list(单向链表)
+ * Class definition for singly-linked list(单向链表类声明)
  */
- class ListNodeB {
- public:
+class ListNodeB
+{
+public:
     int val;
     ListNodeB *next;
 
-    ListNodeB(int val) {
+    ListNodeB(int val)
+    {
         this->val = val;
         this->next = NULL;
     }
 };
 
 /**
- * Class definition for doubly-linked list(双向链表)
+ * Class definition for doubly-linked list(双向链表类声明)
  */
-class DListNode {
+class DListNode
+{
 public:
     int val;
     DListNode *prev, *next;
 
-    DListNode(int val) {
+    DListNode(int val)
+    {
         this->val = val;
         this->prev = this->next = NULL;
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    ListNodeA *node1 = new ListNodeA(4);
+    ListNodeA *node2 = new ListNodeA(5);
+    ListNodeA *node3 = new ListNodeA(1);
+
+    cout << node1->val << std::endl;
+    cout << node1->next << std::endl;
+
+    return 0;
+}
