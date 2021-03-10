077b3396080c772ab40abfd0574e29f40adb2b2a
chacha <867571123@qq.com>
2021-03-10 23:34:05 +0800

    feat: 更新二叉树的前 中 后序遍历内容说明

--- binarytree/binary_tree.cpp
+++ binarytree/binary_tree.cpp
@@ -2,8 +2,9 @@
  * @Author: Chacha
  * @Date: 2018-11-24 13:31:24
  * @Last Modified by: Chacha
- * @Last Modified time: 2021-03-09 19:39:19
+ * @Last Modified time: 2021-03-10 12:02:36
  * @Source: https://leetcode.com/explore/learn/card/data-structure-tree
+ *          https://shimo.im/docs/MAbjWlrqWqU1f72m
  */
 
 #include <iostream>
@@ -78,7 +79,7 @@ vector<int> preorderTraversalDivideConquer(TreeNode *root)
  * 处理过程：
  * 1. 访问节点 P，并将节点 P 入栈
  * 2. 判断节点 P 的左孩子是否为空，若为空，则取栈顶的节点并进行出栈操作，并将栈顶节点的右孩子置为当前的节点 P，
- *    循环至1；若不为空，则将 P 的左孩子置为当前的节点P
+ *    重复步骤1；若不为空，则将 P 的左孩子置为当前的节点P
  * 3. 直到 P 为 NULL并且栈为空，则遍历结束
  *
  */
@@ -153,6 +154,15 @@ void iterativePreOrderTraversal2(TreeNode *root)
  * https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/992/#in-order-traversal
  *
  * 访问顺序：左节点->根节点->右节点
+ *
+ * 根据中序遍历的顺序，对于任一节点，优先访问其左节点，而左节点又可以看作是一个根节点，然后继续访问其左节点子节点，
+ * 直到遇到左孩子为为空的节点才进行访问，然后按相同的规则访问其右子树。
+ *
+ * 遍历步骤：
+ * 1. 从根节点开始，开始遍历
+ * 2. 递归输出直至最左，然后输出（中序先输出左孩子，而中序遍历第一个输出的是其最左叶子节点）
+ * 3. 当到达最左节点的时候，访问右节点
+ *
  */
 
 // Recursive Way(递归方式)
@@ -188,7 +198,16 @@ vector<int> inOrderTraversalDivideConquer(TreeNode *root)
     return result;
 }
 
-// Iterative Way(迭代方式)
+/**
+ * Iterative Way(迭代方式)
+ *
+ * 处理过程：
+ * 对于任一节点 P
+ * 1. 若其左节点不为空，则将节点 P 入栈并将 P 的左节点置为当前的 P，然后对当前节点 P 再进行相同的处理
+ * 2. 若其左节点为空，则取栈顶元素并进行出栈操作，访问该栈顶节点，然后将当前的 P 节点置为栈顶节点的右节点
+ * 3. 直到P为NULL并且栈为空则遍历结束
+ *
+ */
 void iterativeInOrderTraversal(TreeNode *root)
 {
     if (root == NULL)
@@ -261,7 +280,16 @@ vector<int> postOrderTraversalDivideConquer(TreeNode *root)
     return result;
 }
 
-// Iterative Way 1(迭代方式1)
+/**
+ * Iterative Way 1(迭代方式1)
+ *
+ * 第一种思路： 对于任一节点 P，将其入栈，然后沿其左子树一直往下搜索，直到搜索到没有左孩子节点，
+ * 此时该节点出现在栈顶，但此时并不能将其出栈并访问。因为其右孩子节点还未被访问，所以接下来按照
+ * 相同的规则将其右子树进行相同的处理，当访问完其右孩子节点时，该节点又出现在栈顶，此时可以将其
+ * 出栈并访问。这样就保证了正确的访问顺序。可以看出，在这个过程中，每个节点都两次出现在栈顶，只有
+ * 在第二次出现在栈顶时，才能访问它。因此需要多设置一个变量标识该节点该节点是否是第一次出现在栈顶。
+ *
+ */
 void iterativePostOrderTraversal1(TreeNode *root)
 {
     if (root == NULL)
@@ -277,7 +305,7 @@ void iterativePostOrderTraversal1(TreeNode *root)
 
         while (node != NULL)
         {
-            node->isFirst = 1; // When the node be accessed at first
+            node->isFirst = 1; // 当前节点首次被访问
             nStack.push(node);
             node = node->left;
         }
@@ -287,13 +315,13 @@ void iterativePostOrderTraversal1(TreeNode *root)
             node = nStack.top();
             nStack.pop();
 
-            if (node->isFirst == 1)
+            if (node->isFirst == 1) // 第一次出现在栈顶
             {
                 node->isFirst++;
                 nStack.push(node);
                 node = node->right;
             }
-            else if (node->isFirst == 2)
+            else if (node->isFirst == 2) // 第二次出现在栈顶
             {
                 cout << node->val;
                 node = NULL;
@@ -302,7 +330,20 @@ void iterativePostOrderTraversal1(TreeNode *root)
     }
 }
 
-// Iterative Way 2(迭代方式2)
+/**
+ * Iterative Way 2(迭代方式2)
+ *
+ * 第二种思路：要保证根节点在左孩子和右孩子访问之后才能访问，因此对于任一节点 P，先将其入栈。如果 P
+ * 不存在左孩子和右孩子，则可以直接访问；或者 P 存在左孩子或者右孩子，但是其左孩子和右孩子都已经被访问
+ * 过了，则同样可以访问该节点。若非上述两种情况，则将 P 的右孩子和左孩子依次入栈，这样就保证了每次取栈
+ * 顶元素的时候，左孩子在右孩子前面被访问，左孩子和右孩子都在根节点前面被访问。
+ *
+ * 其实后序遍历中当前节点要是想被输出, 有三种情况：
+ * 1. 其左右孩子均为NULL
+ * 2. 其左孩子刚被输出，而其右孩子为NULL
+ * 3. 其右孩子刚被输出
+ *
+ */
 void iterativePostOrderTraversal2(TreeNode *root)
 {
     if (root == NULL)
@@ -332,6 +373,7 @@ void iterativePostOrderTraversal2(TreeNode *root)
         }
         else
         {
+            // 由于栈是先进后出，因此先入右孩子, 再左孩子可以保证递归返回时先遍历左孩子
             if (cur->right != NULL)
             {
                 nStack.push(cur->right);
@@ -345,10 +387,19 @@ void iterativePostOrderTraversal2(TreeNode *root)
 }
 
 /**
- * Binary Tree Level Order Traversal
- * Source: https://shimo.im/docs/MAbjWlrqWqU1f72m/
+ * Binary Tree Level Order Traversal(二叉树层次遍历)
  */
-// Recursive Way 1
+
+/**
+ * 递归方式1
+ *
+ * 打印当前树的第n层
+ *
+ * root: 树的根
+ * n: 希望打印的层次
+ * level: 当前递归层次
+ *
+ */
 int PrintLevel1(TreeNode *root, int n, int level)
 {
 
@@ -367,28 +418,38 @@ int PrintLevel1(TreeNode *root, int n, int level)
     }
 }
 
-// Recursive Way 2
-int PrintLevel2(TreeNode *root, int n, int level)
+/**
+ * 递归方式2
+ *
+ * 采用层次递减的方式，level = n为希望递归的层次，而层次每深入一次，level递减，当level=0时，则说明递归至第n层
+ *
+ * root: 树的根
+ * n: 希望打印的层次
+ *
+ */
+int PrintLevel2(TreeNode *root, int level)
 {
 
     if (root == NULL || level < 0)
     {
         return 0;
     }
-    else if (level == n)
+    else if (level == 0)
     {
         cout << root->val;
         return 1;
     }
     else
     {
-        return PrintLevel2(root->left, n, level - 1) + PrintLevel2(root->right, n, level - 1);
+        return PrintLevel2(root->left, level - 1) + PrintLevel2(root->right, level - 1);
     }
 }
 
 /**
  * Iterative Way 1
- * Use two queues, one queue for current level nodes and another for next level nodes
+ *
+ * 双队列法：一个储存本层的节点，另一个储存下层的节点
+ *
  */
 void LevelOrderDev(TreeNode *root)
 {
