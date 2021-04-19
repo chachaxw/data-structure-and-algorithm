56b0286da1ff728133c2336f232316f361f3d36f
chacha <867571123@qq.com>
2021-04-19 18:11:19 +0800

    feat: 更新寻找数组的中心下标算法内容

--- leetcode/array/find_pivot_index.cpp
+++ leetcode/array/find_pivot_index.cpp
@@ -2,35 +2,43 @@
  * @Author: Chacha
  * @Date: 2018-12-28 18:05:11
  * @Last Modified by: Chacha
- * @Last Modified time: 2021-03-26 18:11:00
+ * @Last Modified time: 2021-04-19 18:10:11
  */
 
-/***********************************************************************************
- * Given an array of integers nums, write a method that returns the "pivot" index of this array.
- * We define the pivot index as the index where the sum of the numbers to the left of the index
- * is equal to the sum of the numbers to the right of the index. If no such index exists,
- * we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+/**
+ * 给你一个整数数组 nums，请编写一个能够返回数组 “中心下标” 的方法。数组 中心下标 是数组的一个下标，
+ * 其左侧所有元素相加的和等于右侧所有元素相加的和。如果数组不存在中心下标，返回 -1 。
+ * 如果数组有多个中心下标，应该返回最靠近左边的那一个。
  *
- * Example 1:
- *  Input:
- *  nums = [1, 7, 3, 6, 5, 6]
- *  Output: 3
- *  Explanation:
- *  The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to
- * the sum of numbers to the right of index 3. Also, 3 is the first index where this occurs.
+ * 注意：中心下标可能出现在数组的两端。
+ *
+ * 示例:
+ *  输入: nums = [1, 7, 3, 6, 5, 6]
+ *  输出: 3
+ *
+ * 解释:
+ *  左侧数之和 (1 + 7 + 3 = 11)，右侧数之和 (5 + 6 = 11) ，二者相等。
  *
  * Note:
- *  1. The length of nums will be in the range [0, 10000].
- *  2. Each element nums[i] will be an integer in the range [-1000, 1000].
+ *  1. nums 的长度范围为 [0, 10000]
+ *  2. 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数
  *
- * Source: https://leetcode.com/problems/find-pivot-index/
-************************************************************************************/
+ * 来源: https://leetcode.com/problems/find-pivot-index/
+ */
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+/**
+ * 题解：
+ * 计算出全部数组的和，当遍历到第i个元素时，设其左侧元素之和为sum，则其右侧元素之和为 total - numsi - sum。
+ * 左右侧元素相等即为 sum = total - numsi - sum，即 2 x sum + numsi = total。
+ *
+ * 当中心索引左侧或右侧没有元素时，即为零个项相加，这在数学上称作「空和」（empty sum）。在程序设计中我们约定「空和是零」。
+ *
+ */
 int pivotIndex(vector<int> &nums)
 {
     int total = 0;
