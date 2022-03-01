3419a21181bee2aaebdcdfd261f37207957edb1d
Chacha <867571123@qq.com>
2022-03-01 11:27:40 +0800

    feat: add fibonacci numbers dynamic programing

---
+++ leetcode/dynamic_programming/dp_fibonacci_numbers.cpp
@@ -0,0 +1,92 @@
+/*
+ * @Author: Chacha
+ * @Date: 2022-03-01 10:21:49
+ * @Last Modified by: Chacha
+ * @Last Modified time: 2022-03-01 11:26:52
+ */
+
+/**
+ * 来源：https://leetcode-cn.com/problems/fibonacci-number/
+ *
+ * 动态规划 - 斐波那契数
+ * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0, F(1) = 1
+ * F(n) = F(n-1) + F(n-2), 其中 n > 1 (1 < n <= 30)
+ *
+ * 示例 1：
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ *
+ * 示例 2：
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ *
+ * 示例 3：
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+private:
+    /* data */
+public:
+    int fib(int n);
+
+    int fib1(int n);
+};
+
+/**
+ * 解法1: 直接利用公式递归
+ *
+ * 时间复杂度: O(2^n)
+ * 空间复杂度: O(n)
+ *
+ */
+int Solution::fib(int n)
+{
+    return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
+
+/**
+ * 解法2: 动态规划
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(1)
+ *
+ */
+
+int Solution::fib1(int n)
+{
+    if (n < 2)
+        return n;
+    int p = 0, q = 0, r = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+}
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+
+    cout << "F(4) = " << s.fib(4) << endl;
+    cout << "F(5) = " << s.fib(5) << endl;
+
+    cout << "F(4) = " << s.fib1(4) << endl;
+    cout << "F(5) = " << s.fib1(5) << endl;
+    return 0;
+}
