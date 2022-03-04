0c8b871657f3d1a46c89b00bd27b9e0c959542f3
Chacha <867571123@qq.com>
2022-03-04 18:43:26 +0800

    feat: add cpp thread examples

--- cpp_basics/thread.cpp
+++ cpp_basics/thread.cpp
@@ -2,7 +2,7 @@
  * @Author: Chacha
  * @Date: 2022-03-03 13:51:04
  * @Last Modified by: Chacha
- * @Last Modified time: 2022-03-03 18:45:52
+ * @Last Modified time: 2022-03-03 18:55:10
  */
 
 /**
@@ -38,35 +38,130 @@
  * 在这里，pthread_exit 用于显式地退出一个线程。通常情况下，pthread_exit() 函数是在线程完成工作后无需继续存在时被调用。
  * 如果 main() 是在它所创建的线程之前结束，并通过 pthread_exit() 退出，那么其他线程将继续执行。否则，它们将在 main() 结束时自动被终止。
  *
+ * 连接和分离线程
+ * 我们可以使用以下两个函数来连接或分离线程：
+ * pthread_join (threadid, status)
+ * pthread_detach (threadid)
+ *
+ * pthread_join() 子程序阻碍调用程序，直到指定的 threadid 线程终止为止。直到指定的 threadid线程终止为止。当创建一个线程时，
+ * 它的某个属性会定义它是否可以连接的（joinable）或者 可分离的（detached）。只有创建时定义为可连接的线程才可以被连接。如果
+ * 线程创建时被定义为可分离，则它永远也不能被连接。
+ *
+ * std::thread
+ * C++ 11 之后添加了新的标准线程库 std::thread，std::thread 在 <thread> 头文件中声明，因此使用 std::thread 时需要包含 在 <thread> 头文件。
+ * std::thread 默认构造函数，创建一个空的 std::thread 执行对象。
+ * #include<thread>
+ * std::thread thread_object(callable)
+ *
+ *
+ *
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
 #include <pthread.h>
 
 using namespace std;
 
 #define NUM_THREADS 5
 
-void *sayHello(void *ars)
+struct thread_data
 {
-    cout << "Hello C++" << endl;
+    int thread_id;
+    char *message;
+};
+
+void *wait(void *t)
+{
+    int i;
+    long tid;
+
+    tid = (long)t;
+
+    sleep(1);
+
+    cout << "Sleeping in thread " << endl;
+    cout << "Thread with id: " << tid << " ...exiting" << endl;
+
+    pthread_exit(NULL);
+};
+
+void *sayHello(void *args)
+{
+    struct thread_data *data;
+
+    data = (struct thread_data *)args;
+
+    cout << "Thread ID: " << data->thread_id << endl;
+    cout << "Message: " << data->message << endl;
+
     return 0;
 };
 
 int main(int argc, char const *argv[])
 {
-    pthread_t tids[NUM_THREADS];
+    pthread_t threads[NUM_THREADS];
+    pthread_t threads1[NUM_THREADS];
+    pthread_attr_t attr;
+    struct thread_data td[NUM_THREADS];
+    void *status;
+
+    // 初始化并设置线程为可连接的（joinable）
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    // for (int i = 0; i < NUM_THREADS; i++)
+    // {
+    //     cout << "创建线程, " << i << endl;
+
+    //     td[i].thread_id = i;
+    //     td[i].message = (char *)"This is Message";
+
+    //     // 参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+    //     int ret = pthread_create(&threads[i], NULL, sayHello, (void *)&((td[i])));
+
+    //     if (ret != 0)
+    //     {
+    //         cout << "pthread_create error: error_code=" << ret << endl;
+    //         exit(-1);
+    //     }
+    // }
 
     for (int i = 0; i < NUM_THREADS; i++)
     {
+        cout << "创建线程, " << i << endl;
+
         // 参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
-        int ret = pthread_create(&tids[i], NULL, sayHello, NULL);
+        int ret = pthread_create(&threads1[i], NULL, wait, (void *)&i);
 
         if (ret != 0)
         {
             cout << "pthread_create error: error_code=" << ret << endl;
+            exit(-1);
         }
     }
+
+    int rc;
+
+    // 删除属性，并等待其他线程
+    pthread_attr_destroy(&attr);
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        rc = pthread_join(threads1[i], &status);
+
+        if (rc)
+        {
+            cout << "Error:unable to join," << rc << endl;
+            exit(-1);
+        }
+
+        cout << "Main: completed thread id: " << i;
+        cout << " exiting with status: " << status << endl;
+    }
+
+    cout << "Main: program exiting." << endl;
 
     // 等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来
     pthread_exit(NULL);
