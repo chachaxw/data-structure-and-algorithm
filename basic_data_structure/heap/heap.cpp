d558907f239188fb47ff9715e7af09fda72377ac
chacha <867571123@qq.com>
2022-03-14 14:17:30 +0800

    feat: update heap data structure

--- basic_data_structure/heap/heap.cpp
+++ basic_data_structure/heap/heap.cpp
@@ -2,7 +2,7 @@
  * @Author: Chacha
  * @Date: 2021-03-03 16:06:05
  * @Last Modified by: Chacha
- * @Last Modified time: 2021-03-03 17:30:01
+ * @Last Modified time: 2022-03-14 00:00:49
  */
 
 /**
@@ -18,6 +18,7 @@
  */
 
 // 通过使用【优先队列】的【压入 push()】和【弹出 pop()】操作，即可完成堆排序，实现代码如下
+#include <iostream>
 #include <queue>
 
 using namespace std;
@@ -25,21 +26,42 @@ using namespace std;
 int main()
 {
     // 初始化小顶堆
-    priority_queue<int, vector<int>, greater<int>> heap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    // 初始化大顶堆
+    priority_queue<int> maxHeap;
 
     // 元素入堆
-    heap.push(1);
-    heap.push(4);
-    heap.push(2);
-    heap.push(6);
-    heap.push(8);
+    minHeap.push(1);
+    minHeap.push(4);
+    minHeap.push(2);
+    minHeap.push(6);
+    minHeap.push(8);
 
-    // 元素出堆（从小到大）
-    heap.pop(); // -> 1
-    heap.pop(); // -> 2
-    heap.pop(); // -> 4
-    heap.pop(); // -> 6
-    heap.pop(); // -> 8
+    // 元素入堆
+    maxHeap.push(1);
+    maxHeap.push(4);
+    maxHeap.push(2);
+    maxHeap.push(6);
+    maxHeap.push(8);
+
+    while (!minHeap.empty())
+    {
+        // 元素出堆（从小到大）
+        cout << minHeap.top() << " ";
+        minHeap.pop();
+    }
+
+    cout << endl;
+
+    while (!maxHeap.empty())
+    {
+        // 元素出堆（从大到小）
+        cout << maxHeap.top() << " ";
+        maxHeap.pop();
+    }
+
+    cout << endl;
 
     return 0;
 }
