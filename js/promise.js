b554bfb729f42ef36d6fe8bf9846a55a2cf8cde1
Chacha <867571123@qq.com>
2022-04-21 12:01:57 +0800

    feat: add promise race

--- js/promise.js
+++ js/promise.js
@@ -2,14 +2,15 @@
  * @Author: Chacha
  * @Date: 2022-04-19 22:33:05
  * @Last Modified by: Chacha
- * @Last Modified time: 2022-04-20 23:28:20
+ * @Last Modified time: 2022-04-21 11:49:24
  */
 
 /**
  * Promise 练习
  *
  */
-
+// Promise.all() 方法接收一个 promise 的 iterable 类型（注：Array，Map，Set都属于ES6的iterable类型）的输入，
+// 并且只返回一个 Promise 实例， 那个输入的所有 promise 的 resolve 回调的结果是一个数组。
 Promise.myAll = (promises) => {
     return new Promise((resolve, reject) => {
         if (!Array.isArray(promises)) {
@@ -35,6 +36,8 @@ Promise.myAll = (promises) => {
     });
 };
 
+// 该Promise.allSettled()方法返回一个在所有给定的 promise 都已经 fulfilled 或 rejected 后的 promise，
+// 并带有一个对象数组，每个对象表示对应的 promise 结果。
 Promise.myAllSettled = (promises) => {
     return new Promise((resolve, reject) => {
         if (!Array.isArray(promises)) {
@@ -86,6 +89,15 @@ Promise.myAllSettled = (promises) => {
     });
 };
 
+// Promise.race(iterable) 方法返回一个 promise，一旦迭代器中的某个promise解决或拒绝，返回的 promise 就会解决或拒绝。
+Promise.myRace = (promises) => {
+    return new Promise((resolve, reject) => {
+        promises.forEach((p) => {
+            Promise.resolve(p).then(resolve).catch(reject);
+        });
+    });
+};
+
 let p1 = Promise.resolve(1);
 let p2 = 2;
 let p3 = new Promise((resolve) => {
@@ -102,3 +114,5 @@ Promise.myAll([p1, p2, p3])
     });
 
 Promise.myAllSettled([p1, p2, p4]).then(console.log);
+
+Promise.myRace([p1, p2, p3]).then(console.log);
